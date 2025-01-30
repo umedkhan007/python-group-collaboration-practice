5a50719d1ae98d71e3c0adadd8682f28d39c4433
umedkhan007 <itsmeumedkhan05@gmail.com>
2025-01-30 11:22:16 +0530

    nested for loop changes

---
+++ nested-for-loop-rizwan.py
@@ -0,0 +1,26 @@
+#Sample data
+users={
+    "Alice":{
+        "addresses":["123 Main St","456 Elm St"],
+        "cities":["New York", "los angeles"],
+        "pincodes":{
+            "New York":10001,
+            "Los Angeles":90001
+        }
+    },
+    "Bob":{
+        "addresses":["789 Oak St"],
+        "cities":["chicago"],
+        "pincodes":{ 
+            "chicago":60007
+        }
+    },
+    "charlie":{
+        "addresses":["101 pine st","202 Maple st"],
+        "cities": ["Houstton","Phoenix"],
+        "pincodes":{
+            "Houston":77001,
+            "Pnoenix":85001
+        }
+    }
+}
